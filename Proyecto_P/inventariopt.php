a7dcf1ae5c31bee6a61c481dd99a95c5f33550bb
msbgod <93262973+msbgod@users.noreply.github.com>
2023-02-22 20:05:32 -0500

    Finalizado

--- Proyecto_P/inventariopt.php
+++ Proyecto_P/inventariopt.php
@@ -16,7 +16,9 @@
       <nav class="navigator col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
 
-          <span class="fs-5 d-none d-sm-inline">Menu</span>
+        <span class="fs-5 d-none d-sm-inline">
+            <img src="./logo1.png" alt="" style="width:80%;">
+          </span>
 
           <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
 
